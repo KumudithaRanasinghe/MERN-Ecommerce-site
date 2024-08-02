a540ff743b52c1b2ff3e45a64f3079acd3c58c42
Kumuditha Ranasinghe <ranasinghe24125@gmail.com>
2024-08-02 16:23:40 +0530

    feat: add newsletter

--- frontend/src/pages/Shop.jsx
+++ frontend/src/pages/Shop.jsx
@@ -3,6 +3,7 @@ import Hero from '../components/Hero/Hero'
 import Popular from '../components/Popular/popular'
 import Offers from '../components/Offers/offers'
 import NewCollections from '../components/NewCollections/newcollections'
+import NewsLetter from '../components/NewsLetter/newsletter'
 
 const Shop = () => {
   return (
@@ -11,6 +12,7 @@ const Shop = () => {
       <Popular/>
       <Offers/>
       <NewCollections/>
+      <NewsLetter/>
     </div>
   )
 }
